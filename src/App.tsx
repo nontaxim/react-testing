d38768a07185b605a86e61431797ba72e13ec631
Nontawat <mix_sk139@hotmail.com>
2024-06-06 14:59:55 +0700

    test: add user-event

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
-import { Application } from './components/application/application';
+// import { Application } from './components/application/application';
+import { Counter } from './components/counter/counter';
 
 function App() {
   return (
     <div className="App">
-     <Application />
+      <Counter />
     </div>
   );
 }
